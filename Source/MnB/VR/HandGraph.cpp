9c91565fed60a5459c51d5d910f9af785c1ca43b
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-06-03 16:10:45 +0900

    레퍼런스 변경

--- Source/MnB/VR/HandGraph.cpp
+++ Source/MnB/VR/HandGraph.cpp
@@ -11,55 +11,55 @@ UVRInputDataConfig::UVRInputDataConfig()
 {
 	{
 		static ConstructorHelpers::FObjectFinder<UInputMappingContext> Asset
-		{TEXT("/Script/EnhancedInput.InputMappingContext'/Game/KDT2/Blueprint/VR/Hands/Input/IMC_Hands.IMC_Hands'")};
+		{ TEXT("/Script/EnhancedInput.InputMappingContext'/Game/MyAssets/VR/Input/IMC_Hands.IMC_Hands'") };
 		check(Asset.Object);
 		InputMappingContext = Asset.Object;
 	}
 	{
 		static ConstructorHelpers::FObjectFinder<UInputAction> Asset
-		{ TEXT("/Script/EnhancedInput.InputAction'/Game/KDT2/Blueprint/VR/Hands/Input/IA_Grab_Left.IA_Grab_Left'") };
+		{ TEXT("/Script/EnhancedInput.InputAction'/Game/MyAssets/VR/Input/IA_Grab_Left.IA_Grab_Left'") };
 		check(Asset.Object);
 		IA_Grab_Left = Asset.Object;
 	}
 	{
 		static ConstructorHelpers::FObjectFinder<UInputAction> Asset
-		{ TEXT("/Script/EnhancedInput.InputAction'/Game/KDT2/Blueprint/VR/Hands/Input/IA_Grab_Right.IA_Grab_Right'") };
+		{ TEXT("/Script/EnhancedInput.InputAction'/Game/MyAssets/VR/Input/IA_Grab_Right.IA_Grab_Right'") };
 		check(Asset.Object);
 		IA_Grab_Right = Asset.Object;
 	}
 	{
 		static ConstructorHelpers::FObjectFinder<UInputAction> Asset
-		{ TEXT("/Script/EnhancedInput.InputAction'/Game/KDT2/Blueprint/VR/Hands/Input/IA_Point_Left.IA_Point_Left'") };
+		{ TEXT("/Script/EnhancedInput.InputAction'/Game/MyAssets/VR/Input/IA_Point_Left.IA_Point_Left'") };
 		check(Asset.Object);
 		IA_Point_Left = Asset.Object;
 	}
 	{
 		static ConstructorHelpers::FObjectFinder<UInputAction> Asset
-		{ TEXT("/Script/EnhancedInput.InputAction'/Game/KDT2/Blueprint/VR/Hands/Input/IA_Point_Right.IA_Point_Right'") };
+		{ TEXT("/Script/EnhancedInput.InputAction'/Game/MyAssets/VR/Input/IA_Point_Right.IA_Point_Right'") };
 		check(Asset.Object);
 		IA_Point_Right = Asset.Object;
 	}
 	{
 		static ConstructorHelpers::FObjectFinder<UInputAction> Asset
-		{ TEXT("/Script/EnhancedInput.InputAction'/Game/KDT2/Blueprint/VR/Hands/Input/IA_IndexCurl_Left.IA_IndexCurl_Left'") };
+		{ TEXT("/Script/EnhancedInput.InputAction'/Game/MyAssets/VR/Input/IA_IndexCurl_Left.IA_IndexCurl_Left'") };
 		check(Asset.Object);
 		IA_IndexCurl_Left = Asset.Object;
 	}
 	{
 		static ConstructorHelpers::FObjectFinder<UInputAction> Asset
-		{ TEXT("/Script/EnhancedInput.InputAction'/Game/KDT2/Blueprint/VR/Hands/Input/IA_IndexCurl_Right.IA_IndexCurl_Right'") };
+		{ TEXT("/Script/EnhancedInput.InputAction'/Game/MyAssets/VR/Input/IA_IndexCurl_Right.IA_IndexCurl_Right'") };
 		check(Asset.Object);
 		IA_IndexCurl_Right = Asset.Object;
 	}
 	{
 		static ConstructorHelpers::FObjectFinder<UInputAction> Asset
-		{ TEXT("/Script/EnhancedInput.InputAction'/Game/KDT2/Blueprint/VR/Hands/Input/IA_Thumb_Left.IA_Thumb_Left'") };
+		{ TEXT("/Script/EnhancedInput.InputAction'/Game/MyAssets/VR/Input/IA_Thumb_Left.IA_Thumb_Left'") };
 		check(Asset.Object);
 		IA_Thumb_Left = Asset.Object;
 	}
 	{
 		static ConstructorHelpers::FObjectFinder<UInputAction> Asset
-		{ TEXT("/Script/EnhancedInput.InputAction'/Game/KDT2/Blueprint/VR/Hands/Input/IA_Thumb_Right.IA_Thumb_Right'") };
+		{ TEXT("/Script/EnhancedInput.InputAction'/Game/MyAssets/VR/Input/IA_Thumb_Right.IA_Thumb_Right'") };
 		check(Asset.Object);
 		IA_Thumb_Right = Asset.Object;
 	}
