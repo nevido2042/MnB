cac960520be4575a00145f90998e4bb96187ca12
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-06-25 14:59:34 +0900

    구현 중

---
+++ Source/MnB/Controller/WorldMapController.cpp
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Controller/WorldMapController.h"
+#include "Blueprint/AIBlueprintHelperLibrary.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+
+void AWorldMapController::BeginPlay()
+{
+	// Call the base class  
+	Super::BeginPlay();
+
+	//Add Input Mapping Context
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(MappingContext, 0);
+	}
+
+	SetShowMouseCursor(true);
+
+	FInputModeGameAndUI Mode;
+	SetInputMode(Mode);
+
+}
+
+void AWorldMapController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+    if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent))
+    {
+        EnhancedInputComponent->BindAction(ClickMoveAction, ETriggerEvent::Completed, this, &AWorldMapController::ClickMove);
+    }
+}
+
+void AWorldMapController::ClickMove()
+{
+	UAIBlueprintHelperLibrary::SimpleMoveToLocation(this, GetClickLocation());
+}
+
+FVector AWorldMapController::GetClickLocation()
+{
+	FHitResult Hit;
+	bool bHitSuccessful = false;
+
+	{
+		bHitSuccessful = GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, true, Hit);
+	}
+
+	// If we hit a surface, cache the location
+	if (bHitSuccessful)
+	{
+		return Hit.Location;
+	}
+
+	return FVector::ZeroVector;
+	//// Move towards mouse pointer or touch
+	//APawn* ControlledPawn = GetPawn();
+	//if (ControlledPawn != nullptr)
+	//{
+	//	FVector WorldDirection = (CachedDestination - ControlledPawn->GetActorLocation()).GetSafeNormal();
+	//	ControlledPawn->AddMovementInput(WorldDirection, 1.0, false);
+	//}
+}
---
+++ Source/MnB/Controller/WorldMapController.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "WorldMapController.generated.h"
+
+/**
+ * 
+ */
+
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class MNB_API AWorldMapController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	virtual void BeginPlay() override;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* MappingContext;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* ClickMoveAction;
+
+	virtual void SetupInputComponent() override;
+
+	void ClickMove();
+
+	FVector GetClickLocation();
+};
